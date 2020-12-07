719d8e587d0d4a0bb5d72a1795a5e360d7252c95
Carlos Camacho <ccamacho90571@ufide.ac.cr>
2020-12-06 19:23:57 -0600

    Cambios en la interfaz

---
+++ app/src/main/java/com/example/proyectofinalambienteweb/activity_OlvidoContrasena.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinalambienteweb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_OlvidoContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__olvido_contrasena);
+    }
+}
